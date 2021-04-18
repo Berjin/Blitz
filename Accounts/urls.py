106a35470bbf3932478c2cf2110c90c4ff54560c
Berjin <berjin1298@gmail.com>
2021-04-18 22:22:09 +0530

    incomplete base

---
+++ Accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('home/',views.home,name='home'),
+    path('', views.customer_login_view,name='customerlogin'),
+    path('dashboard/',views.customer_dashboard, name='customer_dashboard'),
+    path('request/',views.customer_request,name='request'),
+    path('signup/',views.customer_signup,name='customersignup'),
+    path('employeelogin/', views.employee_login_view,name='employeelogin'),
+    path('employeedashboard/',views.employee_dashboard, name='employee_dashboard')
+]
